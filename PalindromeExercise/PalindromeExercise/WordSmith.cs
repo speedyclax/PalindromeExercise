49b5fb49abab0d309367ad358a11c1345ab44fa5
Jon Claxton <speedyclax@gmail.com>
2023-08-24 21:40:04 -0500

    exercise 2 completed

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word) 
+        {
+            for (int i = 0, j = word.Length - 1; i < j; i++, j--) 
+           
+            if (word[i] == word[j])
+             {
+                 return true;
+             }
+             return false;
+          
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("bulldog", false)]
+        [InlineData("mom", true)]
 
+        public void PalindromeTest(string word, bool expected)
+        {
+            var test = new WordSmith();
+
+            var actual = test.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
+
     }
 }
